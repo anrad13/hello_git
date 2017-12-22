0111128c439794e9771ff07fc0e9a01926924461
anrad13 <anrad13@gmail.com>
2017-12-22 15:53:54 +0100

    Commit 4. Add MyClass

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello git 3");
+        System.out.println("Hello git 6");
+        MyClass c = new MyClass();
     }
 }
